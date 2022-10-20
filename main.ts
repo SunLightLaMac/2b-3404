d45b184713ad970bb11e1d1c2d8d99d1f647c05b
SunLightLaMac <115679788+SunLightLaMac@users.noreply.github.com>
2022-10-20 10:37:46 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,15 @@
+let count = 0
+/**
+ * rubbish
+ */
 input.onButtonPressed(Button.A, function () {
-    while (true) {
-        music.playSoundEffect(music.createSoundEffect(
-        WaveShape.Sine,
-        3629,
-        0,
-        255,
-        0,
-        3710,
-        SoundExpressionEffect.None,
-        InterpolationCurve.Linear
-        ), SoundExpressionPlayMode.InBackground)
-        music.changeTempoBy(20)
-        basic.showLeds(`
-            . . . . .
-            . . . . #
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # . # .
-            . . # . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-        basic.showLeds(`
-            . # . # .
-            # . . . #
-            . . . . .
-            # . . . #
-            . # . # .
-            `)
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
-})
-basic.forever(function () {
 	
+})
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    basic.showNumber(count)
+    for (let index = 0; index < 4; index++) {
+        basic.showNumber(count)
+        count += -1
+    }
 })
